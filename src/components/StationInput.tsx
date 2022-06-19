38bb9941ad6a91fe2e00f4ee4894c476069f13c2
edman <62401294+quotpw@users.noreply.github.com>
2022-06-19 17:02:44 +0300

    Adding a station input field and correcting minor grammatical errors in the readme.md file. (#2)
    * fix(StationInput): added StationInput

---
+++ src/components/StationInput.tsx
@@ -0,0 +1,12 @@
+export enum StationInputType {
+    from = 'from',
+    to = 'to'
+}
+
+interface IProps {
+    type: StationInputType
+}
+
+export function StationInput(props: IProps) {
+    return <input id={'station-input-' + props.type}/>
+}
