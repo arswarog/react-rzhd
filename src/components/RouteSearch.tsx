99b5577209fa381ae235175fe6f57ebede111e54
Ledatu <zavoronok.danila@gmail.com>
2022-06-19 16:28:47 +0300

    feat(RouteSearch): Добавлена заглушка RouteSearch

---
+++ src/components/RouteSearch.tsx
@@ -0,0 +1,13 @@
+import './RouteSearch.css';
+import React from 'react';
+
+export function RouteSearch() {
+    return (
+        <div className="RouteSearch">
+            <div>Import Form</div>
+            <div>Arrow</div>
+            <div>Import Form</div>
+            <div>Button</div>
+        </div>
+    );
+}
