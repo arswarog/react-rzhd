9cb83df0b95d95631a722255f480be204e39f1c0
Ledatu <zavoronok.danila@gmail.com>
2022-07-03 15:08:28 +0300

    feat(Button): Added Button component (#4)

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils"
+import { ReactNode } from "react"
+
+export enum ButtonType {
+    text = 'text',
+    contained = 'contained',
+    outlined = 'outlined',
+}
+
+interface IButton {
+    type?: ButtonType
+    onClick?: () => any
+    disabled?: boolean
+    children?: ReactNode
+}
+
+export function Button(props: IButton) {
+    const { type = ButtonType.contained, onClick = () => { }, disabled = false, children } = props
+    return (
+        <button className={`Button-${type}`} onClick={onClick} disabled={disabled}>
+            {children}
+        </button>
+    )
+}
