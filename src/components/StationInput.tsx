58efe6ca6737f33826a727c13044c12c74916087
edman <edgard011012@gmail.com>
2022-07-03 17:13:51 +0300

    - Added onChange props to StationInput

--- src/components/StationInput.tsx
+++ src/components/StationInput.tsx
@@ -1,4 +1,5 @@
 import './StationInput.css';
+import {SyntheticEvent} from "react";
 
 export enum StationInputType {
     src = 'src',
@@ -8,12 +9,13 @@ export enum StationInputType {
 interface IProps {
     type: StationInputType;
     stations: string[];
+    onChange?: (e: SyntheticEvent) => void;
 }
 
-export function StationInput({type, stations}: IProps) {
+export function StationInput({type, stations, onChange}: IProps) {
     return (
         <div className="route-search__input-box">
-            <input name={type} autoComplete="off">
+            <input name={type} autoComplete="off" onChange={onChange}>
                 <ul id={`route-search-${type}-suggest`} className="route-search__suggest route-search__suggest_open">
                     {stations.map(
                         (item) => (<li className="route-search__suggest-item">{item}</li>)
