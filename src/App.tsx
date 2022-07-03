469f8dd3d2ab0189f10928b3b48c64c9aef904b8
Alexander Gudenko <agudenko2006@gmail.com>
2022-07-03 17:35:32 +0300

    feat(TicketList): Deleted old components; created a basic ticket list

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import React, { useState } from 'react';
 
 import './App.css';
 import { Section, SectionLeftContent, SectionRightContent } from './components/Section';
+import { ITicket, TicketList } from './components/TicketList';
 import logo from './logo.svg';
 
-const initialData = [
-    { id: 1, title: '123' },
-    { id: 2, title: '32435' },
-    { id: 3, title: '52' },
-    { id: 4, title: 'dfgdf' },
+const initialTickets = [
+    {
+        src: 'Source',
+        dst: 'Destination',
+        stop_number: 8,
+    },
 ];
 
 function App() {
-    const [data, setData] = useState(initialData);
+    const [tickets, setTickets] = useState(initialTickets);
 
     function addItem() {
-        setData([
-            ...data,
+        setTickets([
+            ...tickets,
             {
-                id: data.length + 1,
-                title: Math.random().toString(36).substring(2),
+                src: 'City1',
+                dst: 'City2',
+                stop_number: 2,
             },
         ]);
     }
@@ -28,34 +31,8 @@ function App() {
 
     return (
         <div className="App">
-            <Section>
-                <SectionLeftContent title="Станции">
-                    <div>hello</div>
-                </SectionLeftContent>
-                <SectionRightContent title="Маршруты">
-                    <ul className="station-list" id="route-list">
-                        <template id="route-list-item">
-                            <li className="station-list__item">
-                                <h4 className="station-list__title" data-source="name">Кисловодск</h4>
-                                <div className="station-list__info">
-                                    <span className="data">
-                                        <span className="data__name">ID:</span>
-                                        <span className="data__value" data-source="id">2</span>
-                                    </span>
-                                </div>
-                            </li>
-                        </template>
-                    </ul>
-                </SectionRightContent>
-            </Section>
-
             <button onClick={addItem}>add item</button>
-
-            <ul>
-                {data.map((item) => (
-                    <li key={item.id}>{item.title}</li>
-                ))}
-            </ul>
+            <TicketList tickets={tickets} />
         </div>
     );
 }
--- src/components/TicketPreview.tsx
+++
@@ -1,8 +0,0 @@
-export function TicketPreview() {
-    return (
-        <div>
-            <p>CityName1 - CityName2</p>
-            <p>TrainStopCount</p>
-        </div>
-    );
-}
