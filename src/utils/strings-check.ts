8ce7946b0cebaa705ee4fccc729d37c536fd9a1a
TzarevAV <tzarev@bk.ru>
2022-07-17 15:51:08 +0300

    string comparison

--- src/utils/strings-check.ts
+++ src/utils/strings-check.ts
@@ -1,25 +1,44 @@
 export function check(a: string, b: string): boolean {
-  if(Math.abs(a.length-b.length)>1)
-   return false;
-  if(Math.abs(a.length-b.length)==1){
-    if(a.length>b.length){
-        for(let i=0; i<b.length; i++){
-            if(b[i]!=a[i])
-             return false;
+    if (Math.abs(a.length - b.length) > 1)
+        return false;
+    if(Math.abs(a.length - b.length) ==0){
+        let cnt = 0;
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                cnt++;
+                if (cnt > 1)
+                    return false;
+            }
         }
+        return true;
     }
-    for(let i=0; i<a.length;i++){
-        if(b[i]!=a[i])
-         return false;
+
+    if (a.length > b.length) {
+        let i=0, j=0, cnt=0;
+        while (i < b.length && j<a.length) {
+           if (b[i] != a[j]){
+                if(cnt==0)
+                    j++;
+                cnt++;
+                if(cnt>1)
+                    return false;
+           }
+           i++;
+           j++;       
+        }
+        return true;
+    } 
+    let i=0, j=0, cnt=0;
+    while (i < b.length && j<a.length) {
+       if (b[i] != a[j]){
+            if(cnt==0)
+                i++;
+            cnt++;
+            if(cnt>1)
+                return false;
+       }
+       i++;
+       j++;       
     }
-  }
-  let cnt=0;
-  for(let i=0; i<a.length; i++){
-    if(a[i]!=b[i]){
-        cnt++;
-        if(cnt>1)
-            return false;
-    }
-  }
-  return true;
+    return true;
 }
