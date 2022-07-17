6d369db977ea8709526240f713d5e9423cdf9e3e
TzarevAV <tzarev@bk.ru>
2022-07-17 15:00:24 +0300

    string comparision

--- src/utils/strings-check.ts
+++ src/utils/strings-check.ts
@@ -1,3 +1,25 @@
 export function check(a: string, b: string): boolean {
-
+  if(Math.abs(a.length-b.length)>1)
+   return false;
+  if(Math.abs(a.length-b.length)==1){
+    if(a.length>b.length){
+        for(let i=0; i<b.length; i++){
+            if(b[i]!=a[i])
+             return false;
+        }
+    }
+    for(let i=0; i<a.length;i++){
+        if(b[i]!=a[i])
+         return false;
+    }
+  }
+  let cnt=0;
+  for(let i=0; i<a.length; i++){
+    if(a[i]!=b[i]){
+        cnt++;
+        if(cnt>1)
+            return false;
+    }
+  }
+  return true;
 }
