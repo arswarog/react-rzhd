032e8410572bebc7bb7eceb5c9b8437bed1bd552
Ledatu <zavoronok.danila@gmail.com>
2022-07-17 15:37:31 +0300

    fix: prettier semicolon endings added

--- src/utils/strings-check.spec.ts
+++ src/utils/strings-check.spec.ts
@@ -1,54 +1,54 @@
-import { check } from "./strings-check"
+import { check } from './strings-check';
 
 describe('String check', () => {
     it('equal', () => {
-        const result = check('qwerty', 'qwerty')
+        const result = check('qwerty', 'qwerty');
 
-        expect(result).toBeTruthy()
-    })
-    it('different ', () => {
-        const result = check('qWERty', 'qREWty')
-
-        expect(result).toBeFalsy()
-    })
-    it('add symbol', () => {
-        const result = check('quwerty', 'qwerty')
-
-        expect(result).toBeTruthy()
-    })
-    it('remove symbol', () => {
-        const result = check('qwerty', 'quwerty')
-
-        expect(result).toBeTruthy()
-    })
-    it('replace symbol', () => {
-        const result = check('qwirty', 'qwerty')
-
-        expect(result).toBeTruthy()
-    })
-    it('more replacements', () => {
-        const result = check('asdfasdf', 'aXdfasYf')
-
-        expect(result).toBeFalsy()
-    })
+        expect(result).toBeTruthy();
+    });
     it('different', () => {
-        const result = check('1', '123')
+        const result = check('qWERty', 'qREWty');
 
-        expect(result).toBeFalsy()
-    })
+        expect(result).toBeFalsy();
+    });
+    it('add symbol', () => {
+        const result = check('quwerty', 'qwerty');
+
+        expect(result).toBeTruthy();
+    });
+    it('remove symbol', () => {
+        const result = check('qwerty', 'quwerty');
+
+        expect(result).toBeTruthy();
+    });
+    it('replace symbol', () => {
+        const result = check('qwirty', 'qwerty');
+
+        expect(result).toBeTruthy();
+    });
+    it('more replacements', () => {
+        const result = check('asdfasdf', 'aXdfasYf');
+
+        expect(result).toBeFalsy();
+    });
+    it('diffs', () => {
+        const result = check('1', '123');
+
+        expect(result).toBeFalsy();
+    });
     it('one symbol', () => {
-        const result = check('1', '2')
+        const result = check('1', '2');
 
-        expect(result).toBeTruthy()
-    })
+        expect(result).toBeTruthy();
+    });
     it('zero symbols', () => {
-        const result = check('1', '')
+        const result = check('1', '');
 
-        expect(result).toBeTruthy()
-    })
+        expect(result).toBeTruthy();
+    });
     it('diff by two and more symbols', () => {
-        const result = check('qweeety', 'qwerty')
+        const result = check('qweeety', 'qwerty');
 
-        expect(result).toBeFalsy()
-    })
-})
+        expect(result).toBeFalsy();
+    });
+});
