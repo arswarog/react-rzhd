e51ff543d4f6803e48b62af069f748a737b6536e
Ledatu <zavoronok.danila@gmail.com>
2022-07-17 15:21:18 +0300

    fix(check): Replaced unnecessary string with counter

--- src/utils/strings-check.ts
+++ src/utils/strings-check.ts
@@ -3,21 +3,21 @@ export function check(a: string, b: string): boolean {
         return false;
     }
 
-    let diff: string = '';
+    let count: number = 0;
 
     for (let i: number = 0; i < Math.min(a.length, b.length); i++) {
         if (a.charAt(i) != b.charAt(i)) {
             if (a.slice(0, i) + a.slice(i + 1, a.length) === b || b.slice(0, i) + b.slice(i + 1, b.length) === a) {
                 return true;
             } else {
-                diff += a.charAt(i);
+                count++;
 
-                if (diff.length > 1) {
+                if (count > 1) {
                     return false;
                 }
             }
         }
     }
 
-    return diff.length <= 1;
+    return count <= 1;
 }
