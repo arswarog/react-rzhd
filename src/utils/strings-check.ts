d37672c7efc6a2645e483cfe800aa5e4c959f163
Ledatu <zavoronok.danila@gmail.com>
2022-07-17 15:17:18 +0300

    feat(check): Working, but creating new strings :)

--- src/utils/strings-check.ts
+++ src/utils/strings-check.ts
@@ -1,3 +1,23 @@
 export function check(a: string, b: string): boolean {
+    if (Math.abs(a.length - b.length) > 1) {
+        return false;
+    }
 
+    let diff: string = '';
+
+    for (let i: number = 0; i < Math.min(a.length, b.length); i++) {
+        if (a.charAt(i) != b.charAt(i)) {
+            if (a.slice(0, i) + a.slice(i + 1, a.length) === b || b.slice(0, i) + b.slice(i + 1, b.length) === a) {
+                return true;
+            } else {
+                diff += a.charAt(i);
+
+                if (diff.length > 1) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return diff.length <= 1;
 }
