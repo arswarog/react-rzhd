eed3d8689e42272162c88f84715cd9fa74f0151f
Alexander Gudenko <agudenko2006@gmail.com>
2022-07-17 15:21:45 +0300

    A working solution. May be suboptimal, though

--- src/utils/strings-check.spec.ts
+++ src/utils/strings-check.spec.ts
@@ -1,49 +1,49 @@
-import { check } from "./strings-check"
+import { check } from './strings-check';
 
 describe('String check', () => {
     it('equal', () => {
-        const result = check('qwerty', 'qwerty')
+        const result = check('qwerty', 'qwerty');
 
-        expect(result).toBeTruthy()
-    })
-    it('different ', () => {
-        const result = check('qWERty', 'qREWty')
-
-        expect(result).toBeFalsy()
-    })
-    it('add symbol', () => {
-        const result = check('quwerty', 'qwerty')
-
-        expect(result).toBeTruthy()
-    })
-    it('remove symbol', () => {
-        const result = check('qwerty', 'quwerty')
-
-        expect(result).toBeTruthy()
-    })
-    it('replace symbol', () => {
-        const result = check('qwirty', 'qwerty')
-
-        expect(result).toBeTruthy()
-    })
-    it('more replacements', () => {
-        const result = check('asdfasdf', 'aXdfasYf')
-
-        expect(result).toBeFalsy()
-    })
+        expect(result).toBeTruthy();
+    });
     it('different', () => {
-        const result = check('1', '123')
+        const result = check('qWERty', 'qREWty');
 
-        expect(result).toBeFalsy()
-    })
+        expect(result).toBeFalsy();
+    });
+    it('add symbol', () => {
+        const result = check('quwerty', 'qwerty');
+
+        expect(result).toBeTruthy();
+    });
+    it('remove symbol', () => {
+        const result = check('qwerty', 'quwerty');
+
+        expect(result).toBeTruthy();
+    });
+    it('replace symbol', () => {
+        const result = check('qwirty', 'qwerty');
+
+        expect(result).toBeTruthy();
+    });
+    it('more replacements', () => {
+        const result = check('asdfasdf', 'aXdfasYf');
+
+        expect(result).toBeFalsy();
+    });
+    it('more different', () => {
+        const result = check('1', '123');
+
+        expect(result).toBeFalsy();
+    });
     it('one symbol', () => {
-        const result = check('1', '2')
+        const result = check('1', '2');
 
-        expect(result).toBeTruthy()
-    })
+        expect(result).toBeTruthy();
+    });
     it('zero symbols', () => {
-        const result = check('1', '')
+        const result = check('1', '');
 
-        expect(result).toBeTruthy()
-    })
-})
+        expect(result).toBeTruthy();
+    });
+});
--- src/utils/strings-check.ts
+++ src/utils/strings-check.ts
@@ -1,3 +1,26 @@
 export function check(a: string, b: string): boolean {
+    if (Math.abs(a.length - b.length) > 1) {
+        return false;
+    }
 
+    if (a.length < b.length) {
+        const t = a;
+
+        a = b;
+        b = t;
+    }
+
+    let counter = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            if (a[i] === b[i - 1]) return true;
+
+            counter++;
+        }
+    }
+
+    if (counter > 1) return false;
+
+    return true;
 }
