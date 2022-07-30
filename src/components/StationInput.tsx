96572d5f984eecbba9ede2601ff32c3641072881
quot <edgard011012@gmail.com>
2022-07-31 00:38:03 +0300

    fix: added type of input

--- src/components/StationInput.tsx
+++ src/components/StationInput.tsx
@@ -2,12 +2,18 @@ import './StationInput.css';
 import { SyntheticEvent } from 'react';
 import { IStation } from '../models/station';
 
+export enum StationInputType {
+    src = 'src',
+    dst = 'dst',
+}
+
 interface IProps {
+    type: StationInputType;
     stations: IStation[];
     onFoundStations?: (stations: IStation[]) => void;
 }
 
-export function StationInput({ stations, onFoundStations }: IProps) {
+export function StationInput({ type, stations, onFoundStations }: IProps) {
     function onChange(e: SyntheticEvent) {
         if (onFoundStations) {
             onFoundStations(
@@ -21,10 +27,14 @@ export function StationInput({ stations, onFoundStations }: IProps) {
     return (
         <div className="route-search__input-box">
             <input
+                name={type}
                 autoComplete="off"
                 onChange={onChange}
             >
-                <ul className="route-search__suggest route-search__suggest_open">
+                <ul
+                    id={`route-search-${type}-suggest`}
+                    className="route-search__suggest route-search__suggest_open"
+                >
                     {stations.map((item) => (
                         <li className="route-search__suggest-item">{item.name}</li>
                     ))}
